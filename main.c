5dd447d298568c63503adae83b77359dddec4443
saffron <saffron@saffron.lan>
2026-02-04 19:03:16 -0700

    Initial commit.

---
+++ main.c
@@ -0,0 +1,42 @@
+#define SAFFRON_IMPLEMENTATION
+#include "saffron.h"
+#include <SDL2/SDL.h>
+
+int main(int argc, char* argv[])
+{
+  const int width = 800;
+  const int height = 600;
+
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_Window* window = SDL_CreateWindow("Saffron Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  SDL_Texture* texture = SDL_CreateTexture(renderer, 
+                                            SDL_PIXELFORMAT_ARGB8888, 
+                                            SDL_TEXTUREACCESS_STREAMING, 
+                                            width, height);
+  sf_ctx_t sf_ctx;
+  sf_init(&sf_ctx, width, height);
+
+  int running = 1;
+  SDL_Event event;
+  while (running) {
+      while (SDL_PollEvent(&event)) {
+          if (event.type == SDL_QUIT) running = 0;
+      }
+
+      sf_fill(&sf_ctx, SF_COLOR_BLUE);
+
+      SDL_UpdateTexture(texture, NULL, sf_ctx.buffer, sf_ctx.w * sizeof(sf_packed_color_t));
+      SDL_RenderClear(renderer);
+      SDL_RenderCopy(renderer, texture, NULL, NULL);
+      SDL_RenderPresent(renderer);
+  }
+
+  sf_destroy(&sf_ctx);
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
