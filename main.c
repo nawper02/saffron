8a457031124a3125ec599cca4f48817683ab3d1a
saffron <saffron@saffron.lan>
2026-02-20 22:30:17 -0700

    temporarily fix segfault -- add clipping to fix properly next

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
   sf_init(&sf_ctx, width, height);
   sf_ctx.camera.pos    = (sf_fvec3_t){0.0f, 0.0f, -5.0f};
   sf_ctx.camera.target = (sf_fvec3_t){0.0f, 0.0f, 0.0f};
-  sf_ctx.camera.P      = sf_make_psp_fmat4(60.0f, (float)width/height, 0.1f, 100.0f);
+  sf_ctx.camera.P      = sf_make_psp_fmat4(70.0f, (float)width/height, 0.1f, 100.0f);
   sf_ctx.camera.V      = sf_make_view_fmat4(sf_ctx.camera.pos, sf_ctx.camera.target, (sf_fvec3_t){0,1,0});
   sf_set_logger(&sf_ctx, sf_logger_console, NULL);
   sf_load_obj(&sf_ctx, "../assets/teapot.obj", "teapot");
